e3727a8cc5f890b836dd0115eeff944e549d9530
Jad Joubran <joubran.jad@gmail.com>
2018-04-05 16:37:48 +0300

    Workbox wizard questionnaire. #3

--- api.js
+++ api.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
+const path = require("path");
 const exec = require("child_process").exec;
+const { join } = require("path");
 
 const options = { maxBuffer: 1024 * 1000000 };
 
@@ -33,6 +35,33 @@ module.exports = {
         }
 
         return res.send({ configExists, workboxInstalled });
+      },
+      "check-ignores": (req, res) => {
+        const query = req.query;
+        let dir = query.dir || "./";
+        dir = path.resolve(dir);
+
+        const isDirectory = source => fs.lstatSync(source).isDirectory();
+
+        const getDirectories = source => {
+          return fs
+            .readdirSync(source)
+            .map(name => join(source, name))
+            .filter(isDirectory)
+            .map(item => item.replace(`${dir}`, ""))
+            .map(
+              item =>
+                item[0] === "/" || item[0] === "\\" ? item.substr(1) : item
+            )
+            .filter(item => !item.startsWith("."));
+        };
+        const dirs = getDirectories(dir);
+        const typicallyIgnored = ["bower_components", "node_modules"];
+
+        const intersection = dirs.filter(
+          e => typicallyIgnored.indexOf(e) !== -1
+        );
+        return res.send({ result: intersection });
       }
     },
     post: {
